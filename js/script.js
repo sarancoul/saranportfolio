b70f7379746f06f9265adcd359e645a61b7da4b9
sarancoul <sc523644@gmail.com>
2023-12-22 16:03:46 +0000

    portfolio

--- js/script.js
+++ js/script.js
@@ -45,3 +45,39 @@ ScrollReveal({
     ScrollReveal().reveal('.accueil-img, .service-contenant, .portfolio-boite, .contact form', { origin : 'bottom'});
     ScrollReveal().reveal('.accueil-contenaire h1, .propos-img', {origin : 'left'});
     ScrollReveal().reveal('.accueil-contenaire p, .propos-contenant', {origin: 'right'});
+
+ ///////////////////partie contact /////////////////////////////////////////////////:
+ const form = document.querySelector("form");
+ const fullName = document.getElementById("name");
+ const email = document.getElementById("email");
+ const phone = document.getElementById("phone");
+ const subject = document.getElementById("subject");
+ const message = document.getElementById("message");
+
+// function sendEmail() {
+//      bodyMessage = `Full Name: ${fullName.value}<br> Email:  ${email.value}<br> Phone Number:  ${phone.value}<br> Message:  ${message.value}<br>`;
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "sc523644@gmail.com",
+//         Password : "118ADDBF3AF95FABB2525A7545D597ADF397",
+//         To : 'sc523644@gmail.com',
+//         From : "sc523644@gmail.com",
+//         Subject : subject.value,
+//         Body : bodyMessage
+//     }).then(
+//       message =>{
+//         if (message == "OK") {
+//             Swal.fire({
+//                 title: "Validé",
+//                 text: "Message envoyer avec succès!",
+//                 icon: "success"
+//               });
+//         }
+//       }
+//     );   
+// }
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     sendEmail();
+// });
